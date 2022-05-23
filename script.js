48d2bf7b0cb4381eda551b740e930180310871e3
khatia8 <khpasikashvili@gmail.com>
2022-05-24 01:41:53 +0400

    feat: add logic, styles

--- script.js
+++ script.js
@@ -6,35 +6,65 @@ const container = document.createElement('div');
 container.classList.add('container');
 document.getElementsByTagName('body')[0].prepend(container);
 
-const generateBookItem = (wrapper, item) => {
+const confirmBtn = document.createElement('button');
+confirmBtn.textContent = 'Confirm order';
+document.getElementsByClassName('cart-container')[0].prepend(confirmBtn);
+
+const order = document.getElementById('order');
+
+confirmBtn.onclick = function () {
+  order.style.display = 'block';
+};
+
+const onCheckboxClick = () => {
+  const gifts = document.getElementsByClassName('gift');
+  let temp = 0;
+  for (let count = 0; count < gifts.length; count++) {
+    if (gifts[count].checked == true) {
+      temp = temp + 1;
+    }
+  }
+  if (temp >= 3) {
+    return false;
+  }
+};
+
+const generateBookItem = (wrapper, item, modal) => {
   const img = document.createElement('img');
   img.src = item.imageLink;
   wrapper.appendChild(img);
 
+  const wrap = document.createElement('div');
+  wrapper.appendChild(wrap);
+
   const author = document.createElement('p');
   author.innerText = item.author;
-  wrapper.appendChild(author);
+  wrap.appendChild(author);
 
   const title = document.createElement('h2');
   title.innerText = item.title;
-  wrapper.appendChild(title);
+  wrap.appendChild(title);
 
   const price = document.createElement('p');
   price.innerText = `Price: $${item.price}`;
-  wrapper.appendChild(price);
+  wrap.appendChild(price);
 
   const description = document.createElement('p');
   description.innerText = item.description;
-  wrapper.appendChild(description);
+  wrap.appendChild(description);
 
   const showMoreBtn = document.createElement('button');
   showMoreBtn.id = 'show-more';
   showMoreBtn.textContent = 'Show more ';
-  wrapper.appendChild(showMoreBtn);
+  wrap.appendChild(showMoreBtn);
 
   const addCartBtn = document.createElement('button');
   addCartBtn.textContent = 'add To Cart';
-  wrapper.appendChild(addCartBtn);
+  wrap.appendChild(addCartBtn);
+
+  showMoreBtn.onclick = () => {
+    modal.style.display = 'block';
+  };
 
   generateCartItem(cartContainer, item, addCartBtn);
 };
@@ -48,46 +78,30 @@ const generateModal = (wrapper, item) => {
   img.src = item.imageLink;
   content.appendChild(img);
 
-  const author = document.createElement('h2');
-  author.innerHTML = item.author;
-  content.appendChild(author);
-
-  const description = document.createElement('p');
-  description.innerHTML = item.description;
-  content.appendChild(description);
+  const wrap = document.createElement('div');
+  content.appendChild(wrap);
 
   const close = document.createElement('span');
   close.id = 'close';
   close.textContent = 'X';
-  content.appendChild(close);
-};
+  wrap.appendChild(close);
 
-const buttonClicksListener = (wrapper) => {
-  const showMore = document.getElementById('show-more');
-  const close = document.getElementById('close');
+  const author = document.createElement('h2');
+  author.innerHTML = item.author;
+  wrap.appendChild(author);
 
-  if (showMore) {
-    showMore.onclick = () => {
-      wrapper.style.display = 'block';
-    };
-  }
+  const description = document.createElement('p');
+  description.innerHTML = item.description;
+  wrap.appendChild(description);
 
-  if (close) {
-    close.onclick = () => {
-      wrapper.style.display = 'none';
-    };
-  }
-
-  window.onclick = (event) => {
-    if (event.target == wrapper) {
-      wrapper.style.display = 'none';
-    }
+  close.onclick = () => {
+    wrapper.style.display = 'none';
   };
 };
 
 const generateCartItem = (wrapper, item, addBtn) => {
   var cartItem = document.createElement('div');
-  cartItem.className = 'd-none';
+  cartItem.classList.add('cart-item');
   wrapper.appendChild(cartItem);
 
   var img = document.createElement('img');
@@ -99,11 +113,12 @@ const generateCartItem = (wrapper, item, addBtn) => {
   cartItem.appendChild(cartPrice);
 
   var removeBtn = document.createElement('p');
+  removeBtn.classList.add('remove');
   removeBtn.textContent = 'X';
   cartItem.appendChild(removeBtn);
 
   addBtn.onclick = function () {
-    cartItem.style.display = 'block';
+    cartItem.style.display = 'flex';
   };
   removeBtn.onclick = function () {
     cartItem.style.display = 'none';
@@ -117,15 +132,20 @@ fetch('./books.json')
   .then((data) => {
     data.forEach((element) => {
       const bookWrapper = document.createElement('div');
-      bookWrapper.classList.add('container');
+      bookWrapper.classList.add('book-container');
       container.appendChild(bookWrapper);
 
       const modalWrapper = document.createElement('div');
       modalWrapper.className = 'modal-wrapper';
       container.appendChild(modalWrapper);
 
-      generateBookItem(bookWrapper, element);
+      generateBookItem(bookWrapper, element, modalWrapper);
       generateModal(modalWrapper, element);
-      buttonClicksListener(modalWrapper);
+
+      window.onclick = (event) => {
+        if (event.target == modalWrapper) {
+          modalWrapper.style.display = 'none';
+        }
+      };
     });
   });
